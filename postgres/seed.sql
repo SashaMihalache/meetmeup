d8f98120db61f0c3e485d5f06d88499b7e99684a
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-02-03 14:01:13 +0200

    create seed script

---
+++ postgres/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, email) VALUES ('john_doe', 'john@doe.com');
+INSERT INTO users (username, email) VALUES ('bob', 'bob@doe.com');
+INSERT INTO users (username, email) VALUES ('jane', 'jane@doe.com');
+
+INSERT INTO meetups (name, description, user_id) VALUES ('Meetup 1', 'Description 1', 1);
+INSERT INTO meetups (name, description, user_id) VALUES ('Meetup 2', 'Description 2', 2);
+INSERT INTO meetups (name, description, user_id) VALUES ('Meetup 3', 'Description 3', 3);
